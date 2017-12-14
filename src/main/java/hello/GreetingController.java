08cfb1b49f0af3e675c527f475bc5ddcbcf48f88
DinossoLate <ignalelop@alum.us.es>
2017-12-14 18:12:51 +0100

    Corregido el error de la clase

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,18 +18,3 @@ public class GreetingController {
     }
 
 }
-
-public class GreetingTranslator {
-	
-	public String sayHelloIn(String lang) {
-		String hello;
-		if ("en".equals(lang)) {
-			hello = "hello";
-		} else if ("es".equals(lang)) {
-			hello = "hola";
-		} else {
-			hello = "no hablo tu idioma";
-		}
-	}
-
-}
